f49a73d0edf3eb0927528a419dc2fc3bec386381
Darren <xueyublue@gmail.com>
2023-12-03 17:00:38 +0800

    initial commit

---
+++ src/main/java/sg/darren/ms/event/tracker/Application.java
@@ -0,0 +1,13 @@
+package sg.darren.ms.event.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
